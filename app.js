79e3eb457418d20dd78c89fdb4791abafe8634e0
Louis-Étienne Girard <contact@louisetiennegirard.fr>
2022-09-23 18:39:24 +0200

    Add auth

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const authRoutes = require('./routes/auth');
+// const saucesRoutes = require('./routes/sauces');
+
+const path = require('path');
+
+mongoose.connect('mongodb+srv://loulou22:Md-22-le@cluster0-zad8o.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussi !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/auth', authRoutes);
+// app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
